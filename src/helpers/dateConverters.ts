1a612560cee6176ad320e6b5baf1cc9046ab7ed3
sergey <zenkovich_so@mail.ru>
2021-05-18 12:10:46 +0700

    fix bug with date

--- src/helpers/dateConverters.ts
+++ src/helpers/dateConverters.ts
@@ -1,9 +1,9 @@
-//Принимает дату в формате "2021-05-03 09:41" и превращает ее в "03.05.2021, 09:41"
+import { format } from 'date-fns';
 
+//Принимает дату и превращает ее в строку формата "03.05.2021, 09:41"
 export const dateConverter = (date:string):string => {
     //Приводим дату к читаемому формату и обрезаем секунды
-    const convertedDate = new Date(date).toLocaleString();
-    console.log(convertedDate.substring(0, convertedDate.length - 3))
-    return convertedDate.substring(0, convertedDate.length - 3);
+    const convertedDate = format(new Date(date), 'dd.MM.yyyy, HH:mm'); 
+    return convertedDate;
 }
 
